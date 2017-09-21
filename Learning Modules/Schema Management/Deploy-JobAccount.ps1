d5b838fa6a1cddd121581158050dfe5cf64302bb
anumjs <anjangsh@microsoft.com>
2017-09-20 18:20:35 -0700

    Update Deploy-JobAccount.ps1
    adding path ..\Common\CatalogAndDatabaseManagement

--- Learning Modules/Schema Management/Deploy-JobAccount.ps1
+++ Learning Modules/Schema Management/Deploy-JobAccount.ps1
@@ -20,6 +20,7 @@ param(
 $ErrorActionPreference = "Stop" 
 
 Import-Module "$PSScriptRoot\..\Common\SubscriptionManagement" -Force
+Import-Module $PSScriptRoot\..\Common\CatalogAndDatabaseManagement -Force
 Import-Module "$PSScriptRoot\..\WtpConfig" -Force
 
 $config = Get-Configuration
